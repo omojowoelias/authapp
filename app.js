eb9a247fa0f7c500c3fa1efef67df8a1965f2553
omojowo elias <obedelias01@gmail.com>
2023-06-13 11:58:59 -0700

    modified all files

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const mongoose = require('mongoose');
 //const cookieParser = require("cookie-parser");
 //const MongoStore = require('connect-mongo')(session);
 const MongoDBsession = require('connect-mongodb-session')(session)
+const ejs = require('ejs');
+const bcrypt = require('bcrypt');
 
 //Gives access to variables set in the .env files via `process.env.VARIABLE_NAME`
 require('dotenv').config();
 
 //built in express parse for http responses
+app.use("view engine", ejs);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 //app.use(cookieParser());
@@ -19,7 +22,7 @@ var logRoutes = require('./routes/loginRoutes.js');
 var regRoutes = require('./routes/registerRoutes.js');
 
 const mongoURI = ("mongodb://127.0.0.1:27017/trainingdb");
-
+const UserModel = require('./models/User.js');
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
@@ -56,7 +59,71 @@ app.get('/Page', (req, res) => {
     res.send('Elias app is up and running!!')
 });
 
+const isAuth = (req, res, next) => {
+    if(req.session.isAuth) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
 
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await Username.findOne({email});
+
+    if(!user) {
+        return res.redirect('/login');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) {
+        return res.redirect('/login');
+    }
+    res.redirect('/dashboard');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    let user = await UserModel.findOne({email});
+    if(user) {
+        return res.redirect('/register');
+    }
+
+    const hashedPass = await bcrypt.hash(password, 12);
+
+    user = new UserModel({
+        username,
+        email,
+        password: hashedPass
+    })
+    await user.save();
+    res.redirect("/login");
+})
+
+
+app.get('/dashboard', isAuth, (req, res) => {
+    res.render('dashboard');
+})
+
+app.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+})
 app.listen(5000, () => {
     console.log('The server is up and running at port 5000!!!')
 });
